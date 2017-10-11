90618b6caa02b93a71560b1fb4408ede20b70f11
ptutak <p.tutak@outlook.com>
2017-10-11 17:24:03 +0200

    perceptron

--- perceptron.py
+++ perceptron.py
@@ -8,14 +8,19 @@ Created on Tue Oct 10 19:39:26 2017
 import numpy as np
 
 class perceptron:
-    def __init__(self,weights, bias, activationFunction):
+    def __init__(self,weights, bias, learnRate, activFunc):
         self._weights=np.array(weights)
         self._bias=bias
-        self.activationFunc=activationFunction
-    def learn(self,inputValues,expectedValues):
-        s=0
-        if len(inputValues)!=len(self._weights) or len(inputValues)!=len(expectedValues):
+        self._learnRate=learnRate
+        self._activFunc=activFunc
+    def learn(self,inputValues,expectedValue):
+        if len(inputValues)!=len(self._weights):
             raise TypeError('Wrong values length')
-        for i in range(len(self._weights)):
-            
-        
+        s=np.dot(self._weights,inputValues)
+        s-=self._bias
+        s=self._activFunc(s)
+        if s!=expectedValue:
+            for i in range(len(self._weights)):
+                self._weights[i]+=self._learnRate*(expectedValue-s)*inputValues[i]
+            self._bias+=self._learnRate*(expectedValue-s)
+
