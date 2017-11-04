1cf4e325f3cce63e73f82200d92c9e489b370eb4
Piotr Tutak <p.tutak@outlook.com>
2017-11-05 00:11:24 +0100

    keras

--- 03-siec/siec-keras.py
+++ 03-siec/siec-keras.py
@@ -9,6 +9,8 @@ Created on Sat Nov  4 14:57:07 2017
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import optimizers
+
 import numpy as np
 
 def rastrigin(x,y):
@@ -22,11 +24,36 @@ with open('training_data.csv','w') as f:
         y=np.random.sample()*4-2
         print('{0},{1},{2}'.format(x,y,rastrigin(x,y)),file=f)
 
+with open('test_data.csv','w') as f:
+    for i in range(1000):
+        x=np.random.sample()*4-2
+        y=np.random.sample()*4-2
+        print('{0},{1},{2}'.format(x,y,rastrigin(x,y)),file=f)
+
 np.random.seed(7)
 
-dataset=np.loadtxt('training_data.csv',delimiter=',')
-inputData = dataset[:,0:2]
-expected = dataset[:,2]
+dataSet=np.loadtxt('training_data.csv',delimiter=',')
+inputData = dataSet[:,0:2]
+expected = dataSet[:,2]
 
-print(inputData)
-print(expected)
+testDataSet=np.loadtxt('test_data.csv',delimiter=',')
+inputTestData = testDataSet[:,0:2]
+expectedTestData = testDataSet[:,2]
+
+model=Sequential()
+model.add(Dense(20, input_dim=2, activation='sigmoid'))
+model.add(Dense(10,activation='sigmoid'))
+model.add(Dense(1,activation='linear'))
+
+
+adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+model.compile(loss='mean_squared_error', optimizer=adam, metrics=['accuracy'])
+
+model.fit(inputData,expected,epochs=100000,batch_size=20)
+
+scores=model.evaluate(inputTestData,expectedTestData)
+
+print(scores)
+
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
--- 03-siec/siec.py
+++ 03-siec/siec.py
@@ -8,6 +8,10 @@ Created on Fri Nov  3 16:30:59 2017
 from perceptron import *
 import numpy as np
 
+import copy
+import time
+import random
+
 def rastrigin(x,y):
     return 20+x**2-10*np.cos(2*np.pi*x)+y**2-10*np.cos(2*np.pi*y)
 
@@ -19,32 +23,60 @@ with open('training_data.csv','w') as f:
         y=np.random.sample()*4-2
         print('{0},{1},{2}'.format(x,y,rastrigin(x,y)),file=f)
 
+with open('test_data.csv','w') as f:
+    for i in range(1000):
+        x=np.random.sample()*4-2
+        y=np.random.sample()*4-2
+        print('{0},{1},{2}'.format(x,y,rastrigin(x,y)),file=f)
+
 np.random.seed(7)
 
-dataset=np.loadtxt('training_data.csv',delimiter=',')
-inputData = dataset[:,0:2]
-expected = dataset[:,2]
+dataSet=np.loadtxt('training_data.csv',delimiter=',')
+inputData = dataSet[:,0:2]
+expected = dataSet[:,2]
 
-print(inputData)
-print(expected)
-
-
-INTERN_LAYERS=3
-LAYERS=[5 for x in range(INTERN_LAYERS)]
+testDataSet=np.loadtxt('test_data.csv',delimiter=',')
+inputTestData = testDataSet[:,0:2]
+expectedTestData = testDataSet[:,2]
 
 
-ACTIV_FUNCS=[SignSigm()(1) for x in range(INTERN_LAYERS)]
-ACTIV_FUNC_DERIVS=[SignSigm().derivative(1) for x in range(INTERN_LAYERS)]
-LEARN_RATES=[0.1 for x in range(INTERN_LAYERS)]
+INTERN_LAYERS=2
+LAYERS=[20,10]
+
+
+ACTIV_FUNCS=[Sigm()(1) for x in range(INTERN_LAYERS)]
+ACTIV_FUNC_DERIVS=[Sigm().derivative(1) for x in range(INTERN_LAYERS)]
+LEARN_RATES=[0.01 for x in range(INTERN_LAYERS)]
 WEIGHTS=[None for x in range(INTERN_LAYERS)]
-BIASES=[1.0 for x in range(INTERN_LAYERS)]
+BIASES=[0.0 for x in range(INTERN_LAYERS)]
 
-mult=Multilayer(
+multOrig=Multilayer(
         [2,*LAYERS,1],
         [ident,*ACTIV_FUNCS,ident],
-        [zero,*ACTIV_FUNC_DERIVS,zero],
+        [zero,*ACTIV_FUNC_DERIVS,one],
         [[1.0],*WEIGHTS,[1.0 for x in range(5)]],
-        [0.0,*LEARN_RATES,0.0],
+        [0.0,*LEARN_RATES,0.01],
         [0.0,*BIASES,0.0]        
         )
 
+error=1.0
+while error>0.0001:
+    multilayer=copy.deepcopy(multOrig)
+    i=0
+    start=time.clock()
+    run=True
+    while(run):
+        samples=list(zip(inputData,expected))
+        while(run and samples):
+            inp=samples.pop(np.random.randint(0,len(samples)))
+            multilayer.learn(inp[0],[inp[1]])
+
+        results=[]
+        for inp in inputData:
+            results.extend(multilayer.process(inp))
+         
+        error=MSE(results,expected)
+        if error<0.0001:
+            run=False    
+        i+=1
+        print("{0:9};{1: 8.5f}".format(i,time.clock()-start),error,sep=';')
