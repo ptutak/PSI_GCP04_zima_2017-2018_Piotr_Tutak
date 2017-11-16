50b9a657c665d498dddd2e06af409039a3d29ea8
ptutak <p.tutak@outlook.com>
2017-11-16 14:05:57 +0100

    good results

--- 03-siec/analyze-results.py
+++ 03-siec/analyze-results.py
@@ -8,7 +8,7 @@ Created on Tue Nov 14 19:21:32 2017
 import os
 import re
 
-pattern=re.compile(r"- .+step - loss: \d+\.\d+ - acc: \d+\.[\de\+]+")
+pattern=re.compile(r"- .+step - loss: (\d+\.\d+) - acc: \d+\.[\de\+]+")
 
 
 for file in os.listdir("."):
@@ -21,6 +21,6 @@ for file in os.listdir("."):
                 for line in f:
                     out=pattern.search(line)
                     if out:
-                        print("epoch: {0} out: {1}".format(i,out.group(0)),file=fOut)
+                        print("epoch: {0} loss: {1}".format(i,out.group(1)).replace('.',','),file=fOut)
                         i+=1
             
