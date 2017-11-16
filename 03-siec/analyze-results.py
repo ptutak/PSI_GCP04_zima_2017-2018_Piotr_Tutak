2d1dc229d0de5166f370d26fc7faa687be184b0a
ptutak <p.tutak@outlook.com>
2017-11-16 22:31:04 +0100

    ok

--- 03-siec/analyze-results.py
+++ 03-siec/analyze-results.py
@@ -7,21 +7,22 @@ Created on Tue Nov 14 19:21:32 2017
 
 import os
 import re
-import matplotlip.pyplot as plt
+import matplotlib.pyplot as plt
 
 pattern=re.compile(r"- .+step - loss: (\d+\.\d+) - acc: \d+\.[\de\+]+")
 pattern_filename=re.compile(r"\[.+\]-lr-[\d\.]+")
+fileList=[]
 for file in os.listdir("."):
     if file.startswith("results") and file.endswith(".txt"):
         print(os.path.join(".", file))
         with open(file,'r') as f:
             i=1
             outputFile="processed-"+file
+            fileList.append(outputFile)
             with open(outputFile,"w") as fOut:
                 print(pattern_filename.search(file).group(0),file=fOut)
                 for line in f:
                     out=pattern.search(line)
                     if out:
-                        print("{0}".format(out.group(1)).replace('.',','),file=fOut)
+                        print("{0}".format(out.group(1)),file=fOut)
                         i+=1
-
---
+++ 03-siec/plot-results.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 21:29:12 2017
+
+@author: PiotrTutak
+"""
+
+import os
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+data01={}
+data001={}
+for file in os.listdir("."):
+    if file.startswith("processed") and file.endswith(".txt"):
+        print(os.path.join(".", file))
+
+        with open(file,'r') as f:
+            etiq=f.readline().strip()
+            if file.endswith("0.01-decay-0.0.txt"):
+                data001[etiq]=[]
+                dataOut=data001[etiq]
+            else:
+                data01[etiq]=[]
+                dataOut=data01[etiq]
+            for line in f:
+                dataOut.append(float(line.strip()))
+
+
+colors=['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive']
+i=0
+legend=[]
+plt.figure(figsize=(10,20))
+plt.figure(1)
+plt.subplot(211)
+for k,v in data001.items():
+    line,=plt.plot(list(range(len(v))),v,colors[i],linewidth=0.1,label=k)
+    legend.append(mp)
+    i+=1
+
+plt.legend(handles=lines)
+plt.axis([0,100000,0,120])
+plt.xlabel('epoch')
+plt.ylabel('MSE')
+plt.title('Zaleznosc bledu od epoki')
+
+
+
+plt.savefig("testPlot.png")
+plt.show()
+
