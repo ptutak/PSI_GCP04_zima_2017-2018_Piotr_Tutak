ce77ac4c1b62e982ab73eb4de56ba438185310c2
ptutak <p.tutak@outlook.com>
2017-12-29 13:42:49 +0100

    prezentacja

--- 06-Kohonen/siec-kohonen.py
+++ 06-Kohonen/siec-kohonen.py
@@ -10,6 +10,7 @@ from copy import deepcopy
 import sys
 
 data=[]
+testData=[]
 letters=set()
 with open('data.txt') as f:
     for line in f:
@@ -17,26 +18,44 @@ with open('data.txt') as f:
         expected=l[0]
         letters.add(expected)
         letter=[float(x) for x in l[-1]]
+        testLetter=list(letter)
+        change=set()
+        while len(change)<5:
+            change.add(np.random.randint(0,len(letter)))
+        for x in change:
+            if testLetter[x]:
+                testLetter[x]=0.0
+            else:
+                testLetter[x]=1.0
         data.append((np.array(letter),expected))
-        
-EPOCHS=40
+        testData.append((np.array(testLetter),expected))
+with open('testData.txt','w') as f:
+    for x in testData:
+        f.write(x[1])
+        f.write(' ')
+        for y in x[0]:
+            f.write(str(int(y)))
+        f.write('\n')
+
+EPOCHS=100
 SIZE=[5,10,15]
-LEARN_RATE=[0.5,0.1, 0.01]
-RADIUS = [1.0,2.0,3.0,4.0]
-#STDOUT=sys.stdout
-#sys.stdout=open('results.txt','w')
+LEARN_RATE=[0.5,0.1,0.01]
+RADIUS = [1.0,2.0,3.0]
+STDOUT=sys.stdout
+sys.stdout=open('results.txt','w')
 try:
     for s in SIZE:
+        np.random.seed(12)
         layerOrig=LayerKohonen((s,s),35,distanceEuklides,None,None,None)
         for l in LEARN_RATE:
             for r in RADIUS:
                 layer=deepcopy(layerOrig)
-                layer.learnFunc=simpleLearnCorrection(EPOCHS*24)
+                layer.learnFunc=simpleLearnCorrection(EPOCHS*20)
                 layer.learnRate=l
                 layer.actualLearnRate=l
                 layer.radiusFunc=radiusGauss(r,distanceEuklides)
+                np.random.seed(12)
                 for i in range(EPOCHS):
-                    np.random.seed(12)
                     samples=deepcopy(data)
                     np.random.shuffle(samples)
                     while(samples):
@@ -52,20 +71,47 @@ try:
                     results[res['coords']].add(x[1])
                     if len(results[res['coords']])>maxLen:
                         maxLen=len(results[res['coords']])
-                print(i,s,r,l,layer.actualLearnRate,layer.learnFunc.__name__)
-                
+                print("size: {0}, radius: {1}, learningRate: {2}, endLearningRate: {3}".format(s,r,l,layer.actualLearnRate),"learnFunc:",layer.learnFunc.__name__)
+                print('DATA:')
                 formatStr="{0:^"+str(maxLen)+"}  "
                 toPrint=""
                 for res in sorted(list(results.items())):
                     let="".join(sorted(res[1]))
                     if not let:
-                        let="0"
+                        let="."
                     toPrint+=formatStr.format(let)
                     if res[0][1]==s-1:
                         toPrint+="\n"
                 print(toPrint)
-                print()
+                print('TEST DATA:')
+                
+                testResults=dict()
+                for neuron in layer:
+                    testResults[neuron['coords']]=set()
+                maxLen=0
+                acc=0
+                i=0
+                for x in testData:
+                    res=layer.processKohonen(x[0])
+                    if res['coords']==layer.processKohonen(data[i][0])['coords']:
+                        acc+=1
+                    i+=1
+                    testResults[res['coords']].add(x[1])
+                    if len(testResults[res['coords']])>maxLen:
+                        maxLen=len(testResults[res['coords']])
+                formatStr="{0:^"+str(maxLen)+"}  "
+                toPrint=""
+                for res in sorted(list(testResults.items())):
+                    let="".join(sorted(res[1]))
+                    if not let:
+                        let="."
+                    toPrint+=formatStr.format(let)
+                    if res[0][1]==s-1:
+                        toPrint+="\n"
+                print(toPrint)
+                
+                print('Accuracy:',acc/20*100,"%\n")
 finally:
-#    sys.stdout.close()
-#    sys.stdout=STDOUT
+    sys.stdout.close()
+    sys.stdout=STDOUT
     pass
